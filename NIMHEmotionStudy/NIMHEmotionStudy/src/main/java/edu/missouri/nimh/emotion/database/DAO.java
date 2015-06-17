51c16155508f436e6f6c0fd1bb54e9b60aeea5da
Andrew Smith <smith.andrew.l@gmail.com>
2015-06-17 14:07:27 -0500

    Added functions to insert event and locationData records into the database.

--- NIMHEmotionStudy/NIMHEmotionStudy/src/main/java/edu/missouri/nimh/emotion/database/DAO.java
+++ NIMHEmotionStudy/NIMHEmotionStudy/src/main/java/edu/missouri/nimh/emotion/database/DAO.java
@@ -1,10 +1,17 @@
 package edu.missouri.nimh.emotion.database;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import edu.missouri.nimh.emotion.location.LocationBroadcast;
 
 /**
  * @author Andrew Smith
@@ -13,57 +20,115 @@ import org.json.JSONObject;
  */
 public class DAO {
     private final DatabaseHelper helper;
+    private SQLiteDatabase db;
 
     public DAO(Context context) {
         helper = new DatabaseHelper(context);
-        helper.getWritableDatabase();
+        db = helper.getWritableDatabase();
     }
+
+    // *************************** Functions which emulate existing CSV functions ****************
+    protected void writeSurveyToDatabase(String survey, HashMap<String, List<String>> surveyData) {
+
+    }
+
+    public boolean writeLocationToDatabase(Location location, String type) {
+
+        Date time = Calendar.getInstance().getTime();
+
+        double latitude  = location.getLatitude();
+        double longitude = location.getLongitude();
+        float  accuracy  = location.getAccuracy();
+        String provider  = location.getProvider();
+
+        db.beginTransaction();
+
+        long result = insertLocation(latitude,longitude,accuracy,provider, type);
+
+        if(result == -1) {
+            return false;
+        }
+
+        result = insertEvent(LocationBroadcast.ID, time, "?", 0, null, null, null, null, result, null);
+
+        if(result != -1) {
+            db.setTransactionSuccessful();
+        }
+
+        db.endTransaction();
+        return result != -1;
+    }
+
+
+    // ******************** Functions which insert only their parameters into the database **********************
+
 
     /**
-     * This method should write the phones current GPS information into the database
+     * Inserts locoation data into the location table.
+     *
+     * @param latitude   The latitude of the location
+     * @param longitude  The longitude of the location
+     * @param accuracy   The accuracy of the measurement
+     * @param provider   The source of the location information
+     * @param type       The users activity type
+     * @return returns a row id on success or -1 on failure
      */
-    private void writeLocationEvent() {
+    public long insertLocation(double latitude, double longitude, float accuracy, String provider, String type) {
+        ContentValues values = new ContentValues();
+        values.put("lattitude", latitude);
+        values.put("longitude", longitude);
+        values.put("unknown1",  accuracy);
+        values.put("unknown2", provider);
+        values.put("type",      type);
 
-    }
+        db.beginTransaction();
 
-    /**
-     * This method should write information about a change in hardware status to the database
-     */
-    private void writeHardwareInfo() {
+        long result = db.insert("locationData", null, values);
 
-    }
+        if(result!= -1) {
+            db.setTransactionSuccessful();
+        }
 
-    private void writeReport() {
+        db.endTransaction();
 
-    }
-
-    /**
-     * @return Returns a JSON array of all of the data which has not been sent to the server.
-     */
-    public JSONArray getDataToSync() { return null; }
-
-    public JSONObject getReportsToSync()            { return null;  }
-    public JSONObject getLocationEventsToSync()     { return null;  }
-    public JSONObject getHardwareInfoEventsToSync() { return null;  }
-
-    public JSONObject getSubmissionsToSync() { return null; }
-
-    // JSON format is {type: report_submission, submission:{ userId, timestamp, report, answers: [id:answer]}
-
-    // JSON format is submission { userId, timestamp, report }
-    public JSONObject getSubmission(int id) { return null; }
-
-
-    // *************************************************** Data to JSON ********************************
-    public JSONObject eventAsJSON() {
-
-        return null;
+        return result;
     }
 
 
+    public long insertEvent(String userId, Date timestamp, String type, int studyDay, Date scheduledTS, Date startTS, Date endTS, Long surveySubmissionId, Long locationDataId, Long hardwareInfoId) {
+
+        ContentValues values = new ContentValues();
+
+        db.beginTransaction();
+
+        values.put("userId", userId);
+        values.put("timestamp", timestamp.toString());
+        values.put("type", type);
+        values.put("studyDay", studyDay);
+        values.put("scheduledTS", scheduledTS.toString());
+        values.put("startTS", startTS.toString());
+        values.put("endTS", endTS.toString());
+        values.put("surveySubmissionId", surveySubmissionId);
+        values.put("locationDataId", locationDataId);
+        values.put("hardwareInfo", hardwareInfoId);
+
+        long result = db.insert("event", null, values);
+
+        if(result != -1) {
+            db.setTransactionSuccessful();
+        }
+
+        db.endTransaction();
+
+        return result;
+    }
+
+    // *************************************** Data to JSON ********************************
 
 
+    //               Functions which load data and return them as JSON will be here.
 
-// *************************************************** Data to JSON ********************************
+
+   // **************************************** Data to JSON ********************************
 
 }
